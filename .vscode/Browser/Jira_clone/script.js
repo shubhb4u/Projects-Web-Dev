7f165ad012f2182d1d8c17ec382628f7a7e5b8d8
shubhb4u <shubhammazumdar94@gmail.com>
2022-08-03 10:43:48 -0500

    New

---
+++ .vscode/Browser/Jira_clone/script.js
@@ -0,0 +1,18 @@
+let addBtn = document.querySelector(".add-btn");
+console.log(addBtn);
+
+var isModalPresent = false;
+addBtn.addEventListener("click",function(){
+
+
+    // Case 1 - If modal is not present then display modal.
+    if(!isModalPresent){
+
+    }
+
+    // Case2 - If present hide it.
+    else if(isModalPresent){
+
+    }
+    isModalPresent = !isModalPresent;
+})
