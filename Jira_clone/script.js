930164bdd7face78f7391ae42b70a3a4eaef7592
shubhb4u <shubhammazumdar94@gmail.com>
2022-08-04 13:09:18 -0500

    4th Aug

--- Jira_clone/script.js
+++ Jira_clone/script.js
@@ -6,14 +6,24 @@ let addBtn = document.querySelector(".add-btn");
 // Similarly get the object which is affected by the click
 // and provide the required behaviour in the function below.
 const modalCont = document.querySelector(".modal-cont");
+
+// Getting the text area details while using shift to create new ticket,
+// and setting the priorities of the colors in this array based on index.
 const textArea = document.querySelector(".textarea-cont");
+const colors = ["lightpink", "lightgreen", "lightblue", "black"];
+let modalPriorityColor = colors[colors.length - 1];
+
+const mainCont = document.querySelector(".main-cont");
 
 
 var isModalPresent = false;
 
-// Kis event pe trigger karna hai ?? - jab addBtn lo "Click" karenge tab
-addBtn.addEventListener("click",function(){
+//1.--> Kis event pe trigger karna hai ?? - jab addBtn lo "Click" karenge tab
+addBtn.addEventListener("click",function(e){
 
+    // This e object event represents what type of action has occured
+    //  and brings other very inportant information toolbar.
+    console.log(e);
 
     // Case 1 - If modal is not present then display modal.
     if(!isModalPresent){
@@ -30,7 +40,45 @@ addBtn.addEventListener("click",function(){
 
     }
     isModalPresent = !isModalPresent;
-})
+});
+
+//2. --> Create a new ticket when shift is pressed. 
+modalCont.addEventListener("keydown", function(e){
+
+    // The value of key in this field will show which button is pressed.
+    console.log(e);
+
+    if(e.key == "Shift"){
+
+        //1. Call Create ticket function
+        console.log(textArea.value);
+        createTicket(modalPriorityColor, textArea.value);
+
+        //2 alter display and update isModalPresent
+        modalCont.style.display = "none";
+        isModalPresent = false;
+        textArea.value = "";
+    }
+});
+
+function createTicket(ticketColor, data) {
+    
+  
+    // CreateElement is used to create new div/or any other for new ticket
+    let ticketCont = document.createElement("div");
+    ticketCont.setAttribute("class", "ticket-cont");
+    // see at 1:14:00(key = value pair basically like [class = black])
+    
+    ticketCont.innerHTML = `
+          <div class="ticket-color ${ticketColor}"></div>
+          <div class="ticket-id">#${id}</div>
+          <div class="task-area">${data}</div>
+          <div class="ticket-lock">
+              <i class="fa-solid fa-lock"></i>
+          </div>
+      `;
+    mainCont.appendChild(ticketCont);
+
 
 //Hovering over toolbox-color-cont which changes its color
 
