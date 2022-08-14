08b6e0da1b726fb7ef7f2a3ba11ca39b746dfad0
shubhb4u <shubhammazumdar94@gmail.com>
2022-08-14 11:53:19 -0500

    14th Aug

---
+++ script.js
@@ -0,0 +1,26 @@
+let recordBtn = document.querySelector(".record-btn");
+let recordBtnCont = document.querySelector(".record-btn-cont");
+let captureBtn = document.querySelector(".capture-btn");
+let captureBtnCont = document.querySelector(".capture-btn-cont");
+let timerCont = document.querySelector(".timer-cont");
+let timer = document.querySelector(".timer");
+
+/*Case 1 --> Start recording animation of record button */
+
+let isRecording = false;
+recordBtnCont.addEventListener("click", function () {
+
+    if (!isRecording) {
+        //we have to record 
+        recordBtn.classList.add("scale-record");
+        timer.style.display = flex;
+    } else {
+        //stop the recording 
+        recordBtn.classList.remove("scale-record");
+        timer.style.display = none;
+
+    }
+    
+    isRecording = !isRecording;
+  
+});
