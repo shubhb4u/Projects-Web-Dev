37830918f98c3bfc4ebbe1cc6f94c9a5552069cb
shubhb4u <shubhammazumdar94@gmail.com>
2023-04-28 10:23:56 -0500

    Clever

---
+++ Clever/youtube-clone/src/RecommendedVideos.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import './RecommendedVideos.css'
+import VideoCard from './VideoCard';
+import './VideoCard.css';
+
+function RecommendedVideos() {
+  return (
+    <div className="recommendedVideos">
+        <h2>Recommended</h2>
+        <div className="recommendedVideos__videos">
+          <VideoCard
+          title="How to become a web | developer in 100 days"
+          views = "2.3M views"
+          timestamp ="3 days ago"
+          channelImage = "https://yt3.googleusercontent.com/MeY_fGNrjVLV0PVOBN7dRWzMBS0y41YGm55LOaJ02cXV82a7Np9pYxxhHFqdYdncEy1I2cYR=s176-c-k-c0x00ffffff-no-rj"
+          channel="Harikrat"
+          image="https://i.ytimg.com/vi/YLpCPo0FDtE/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD499vA5Idzy4H1mcr_wLgEnjDY8Q"
+          />
+
+          <VideoCard
+          title="World at war | When will this war end ?"
+          views = "100k views"
+          timestamp ="3 days ago"
+          image = "https://i.ytimg.com/vi/-jFYdg31Jd0/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAfCbDgPkMnVEQ1piY15HTAozRTBw"
+          channel="Wion"
+          channelImage="https://yt3.ggpht.com/oQ2NQMuq-9IS6_MZdsGXa0RWecaACremA01Z7jo-YpoEF1H6PyUF8PZzXkV10OYwSP3UMJDeTg=s68-c-k-c0x00ffffff-no-rj"
+          />
+
+          <VideoCard
+          title="PMPL South East Asia"
+          views = "5.3M views"
+          timestamp ="3 days ago"
+          image="https://i.ytimg.com/vi/eGThTHldfS8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAQcbMRUu5BND360O7M6dYyYN2Lew"
+          channel="Jonathan Gaming"
+          channelImage="https://i.ytimg.com/vi/Co3jhNSkUK8/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBqKO85EtfyebbMPi9V49-yq11wbw"
+          />
+
+          <VideoCard
+          title="How to become a web | developer in 100 days"
+          views = "1.1M views"
+          timestamp ="3 days ago"
+          image="https://i.ytimg.com/vi/hTQTuwbrVVg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAWOjLVFKiUfxWfrcU2Jr3jbNna0g"
+          channel="SinghInUSA"
+          channelImage="https://i.ytimg.com/vi/Co3jhNSkUK8/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBqKO85EtfyebbMPi9V49-yq11wbw"
+          />
+
+          <VideoCard
+          title="World at war | When will this war end ?"
+          views = "100k views"
+          timestamp ="3 days ago"
+          image = "https://i.ytimg.com/vi/-jFYdg31Jd0/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAfCbDgPkMnVEQ1piY15HTAozRTBw"
+          channel="Wion"
+          channelImage="https://yt3.ggpht.com/oQ2NQMuq-9IS6_MZdsGXa0RWecaACremA01Z7jo-YpoEF1H6PyUF8PZzXkV10OYwSP3UMJDeTg=s68-c-k-c0x00ffffff-no-rj"
+          />
+
+
+          <VideoCard
+          title="How to become a web | developer in 100 days"
+          views = "1.1M views"
+          timestamp ="3 days ago"
+          image="https://i.ytimg.com/vi/hTQTuwbrVVg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAWOjLVFKiUfxWfrcU2Jr3jbNna0g"
+          channel="SinghInUSA"
+          channelImage="https://i.ytimg.com/vi/Co3jhNSkUK8/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBqKO85EtfyebbMPi9V49-yq11wbw"
+          />
+
+          <VideoCard
+          title="How to become a web | developer in 100 days"
+          views = "2.3M views"
+          timestamp ="3 days ago"
+          channelImage = "https://yt3.googleusercontent.com/MeY_fGNrjVLV0PVOBN7dRWzMBS0y41YGm55LOaJ02cXV82a7Np9pYxxhHFqdYdncEy1I2cYR=s176-c-k-c0x00ffffff-no-rj"
+          channel="Harikrat"
+          image="https://i.ytimg.com/vi/YLpCPo0FDtE/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD499vA5Idzy4H1mcr_wLgEnjDY8Q"
+          />
+
+          <VideoCard
+          title="PMPL South East Asia"
+          views = "5.3M views"
+          timestamp ="3 days ago"
+          image="https://i.ytimg.com/vi/eGThTHldfS8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAQcbMRUu5BND360O7M6dYyYN2Lew"
+          channel="Jonathan Gaming"
+          channelImage="https://i.ytimg.com/vi/Co3jhNSkUK8/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBqKO85EtfyebbMPi9V49-yq11wbw"
+          />
+          
+        </div>
+    </div>
+  )
+}
+
+export default RecommendedVideos
